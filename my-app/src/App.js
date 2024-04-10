3163b51d51f1bd6514eb37da05eb55e7aaf04019
Dane Watts <wattsd1985@gmail.com>
2024-04-10 02:24:28 -0400

    3 Page Setup

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import ErrorPage from "./components/ErrorPage";
+import Services from "./components/Services";
 
 function App() {
   return (
@@ -14,10 +15,14 @@ function App() {
         <Link className="nav-links-about" to="/about">
           About
         </Link>
+        <Link className="nav-links-services" to="/services">
+          Services
+        </Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/about" element={<Services />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </Router>
