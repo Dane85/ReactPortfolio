981f1de10611c883e1bb409c74719a40135ff99a
Dane Watts <wattsd1985@gmail.com>
2024-04-20 02:03:00 -0400

    Links

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -7,37 +7,58 @@ import Lottie from "lottie-react";
 import Layers from "../assets/animations/Layers.json";
 import logotrans from "../assets/images/logotrans.png";
 import MarqueeComponent from "../components/Marquee.js";
-// importing aos
+// Importing AOS library for animation
 import AOS from "aos";
 import "aos/dist/aos.css";
 
 export default function Home() {
+  // Initialize AOS library for animations on component mount
   useEffect(() => {
     AOS.init();
   }, []);
+
   return (
     <main>
+      {/* Hero section */}
       <Container fluid className="heroContainer">
         <Container>
           <Row>
+            {/* Column for hero text and logo */}
             <Col
               md={6}
               className="heroWordsCol"
+              // Animation for fading in from the right
               data-aos="fade-right"
               data-aos-duration="2000"
             >
               <h1>Epic Expressions</h1>
               <h3>Digital Marketing Solutions</h3>
+              {/* Logo image */}
               <img className="heroLogo" src={logotrans} alt="Logo" />
             </Col>
-            <Col md={6} data-aos="fade-left" data-aos-duration="2000">
+            {/* Column for hero animation */}
+            <Col
+              md={6}
+              // Animation for fading in from the left
+              data-aos="fade-left"
+              data-aos-duration="2000"
+            >
+              {/* Lottie animation component */}
               <Lottie className="layers" animationData={Layers} />
             </Col>
           </Row>
         </Container>
       </Container>
-      <Container className="intro" data-aos="fade-up" data-aos-duration="2000">
+
+      {/* Introduction section */}
+      <Container
+        className="intro"
+        // Animation for fading up
+        data-aos="fade-up"
+        data-aos-duration="2000"
+      >
         <Row>
+          {/* Introduction paragraph */}
           <p>
             Welcome to my portfolio! I'm Dane, an avid web development
             enthusiast committed to shaping extraordinary digital encounters. I
@@ -52,9 +73,19 @@ export default function Home() {
           </p>
         </Row>
       </Container>
-      <Container fluid data-aos="fade-up" data-aos-duration="2000">
+
+      {/* Marquee component */}
+      <Container
+        fluid
+        // Animation for fading up
+        data-aos="fade-up"
+        data-aos-duration="2000"
+      >
+        {/* Marquee scrolling component */}
         <MarqueeComponent />
       </Container>
+
+      {/* Footer */}
       <Footer />
     </main>
   );
