9c65c3cb51012cbbdc341607024ec664e48eaca9
Dane Watts <wattsd1985@gmail.com>
2024-04-21 03:09:12 -0400

    lnk correction

--- my-app/src/pages/NotFound.js
+++ my-app/src/pages/NotFound.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Footer from "../components/Footer";
 import Lottie from "lottie-react";
 import animation404 from "../assets/animations/404.json"; // Assuming the animation file is named 404.json
